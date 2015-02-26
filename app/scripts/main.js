2642fac06b2992a41b2c50303631bde6ac4a5039
KostyaD <thedamaxstudio@gmail.com>
2015-02-26 14:17:36 +0300

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,29 @@ Gastal.faq = function() {
         $(this).find('.js-faq-answer').slideToggle();
     });
 }
+Gastal.map = function() {
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(55.724054, 37.610413),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControl: false,
+            scrollwheel: false,
+            disableDoubleClickZoom: true
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        var myLatlng = new google.maps.LatLng(55.724054, 37.610413);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            icon: 'images/location_mark.png'
+        });
+    }
+    if($('#map').length) {
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
+}
 $(function(){
     Gastal.faq();
+    Gastal.map();
 });
--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-var Gastal={};Gastal.faq=function(){$(".js-faq-question").on("click",function(){$(this).toggleClass("opened").siblings().removeClass("opened").find(".js-faq-answer").slideUp(),$(this).find(".js-faq-answer").slideToggle()})},$(function(){Gastal.faq()});
